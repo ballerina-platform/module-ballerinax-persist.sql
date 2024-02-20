a49473779aa17e936594fe6dd8b1d3953ef7b6db
Haritha Hasathcharu <harithahasathcharu@icloud.com>
2024-02-20 12:05:33 +0530

    revert some changes and code cleanup

--- compiler-plugin/src/main/java/io/ballerina/stdlib/persist/sql/compiler/utils/Utils.java
+++
@@ -1,7 +0,0 @@
-package io.ballerina.stdlib.persist.sql.compiler.utils;
-
-public class Utils {
-    public static String stripEscapeCharacter(String name) {
-        return name.startsWith("'") ? name.substring(1) : name;
-    }
-}
